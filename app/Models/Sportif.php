8690fcbd9b21e8f328be17d389c46393b0ad9d6c
ayouberrak <ayouberrak30@gmail.com>
2026-01-10 12:55:21 +0100

    [Models] Update Sportif model

--- app/Models/Sportif.php
+++ app/Models/Sportif.php
@@ -2,5 +2,24 @@
 namespace App\Models;
 
 class Sportif extends User {
-    // Sportif specific logic
+
+    private int $id_sportif;
+    private int $numero_telephone;
+
+    public function __construct(int $id, string $firstName, string $lastName, string $email, string $passwordHash, int $role,
+                                int $id_sportif, int $numero_telephone) {
+        parent::__construct($id, $firstName, $lastName, $email, $passwordHash, $role);
+        $this->id_sportif = $id_sportif;
+        $this->numero_telephone = $numero_telephone;
+    }
+    public function getIdSportif(): int {
+        return $this->id_sportif;
+    }
+    public function getNumeroTelephone(): int {
+        return $this->numero_telephone;
+    }
+    public function setNumeroTelephone(int $numero_telephone): void {
+        $this->numero_telephone = $numero_telephone;
+    }
+
 }
