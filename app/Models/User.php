2c3d73681499c608482e5763c1b8a5ad7949f853
ayouberrak <ayouberrak30@gmail.com>
2026-01-10 12:55:22 +0100

    [Models] Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,5 +2,53 @@
 namespace App\Models;
 
 class User {
-    // Base User Class
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+    private string $email;
+    private string $passwordHash;
+    private int $role;
+
+    public function __construct(int $id , string $firstName, string $lastName, string $email, string $passwordHash, int $role) {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+        $this->passwordHash = $passwordHash;
+        $this->role = $role;
+    }
+    public function getId(): int {
+        return $this->id;
+    }
+    public function getFirstName(): string {
+        return $this->firstName;
+    }
+    public function getLastName(): string {
+        return $this->lastName;
+    }
+    public function getEmail(): string {
+        return $this->email;
+    }
+    public function getPasswordHash(): string {
+        return $this->passwordHash;
+    }
+    public function getRole(): int {
+        return $this->role;
+    }
+    public function setFirstName(string $firstName): void {
+        $this->firstName = $firstName;
+    }
+    public function setLastName(string $lastName): void {
+        $this->lastName = $lastName;
+    }
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+    public function setPasswordHash(string $passwordHash): void {
+        $this->passwordHash = $passwordHash;
+    }
+    public function setRole(int $role): void {
+        $this->role = $role;
+    }
+    
 }
