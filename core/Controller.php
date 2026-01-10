0768d0ce4863ddb41b79a133ebe45296640106d3
ayouberrak <ayouberrak30@gmail.com>
2026-01-10 12:55:22 +0100

    [Core] Update base Controller

--- core/Controller.php
+++ core/Controller.php
@@ -2,21 +2,21 @@
 namespace Core;
 
 class Controller {
+    
     protected $twig;
 
     public function __construct() {
-        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../app/Views');
-        $this->twig = new \Twig\Environment($loader, [
-            'cache' => false,
-            'debug' => true,
-        ]);
 
-        // Calculate base URL
-        $scriptName = $_SERVER['SCRIPT_NAME']; // e.g., /coachPro_v3/public/index.php
-        $dir = dirname($scriptName);           // e.g., /coachPro_v3/public
-        $dir = str_replace('\\', '/', $dir);   // Normalize for Windows
-        if ($dir === '/') $dir = '';           // Handle root case
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../app/Views');
         
+        $this->twig = new \Twig\Environment($loader, [
+            'cache' => false,  
+            'debug' => true,   
+        ]);
+        $scriptName = $_SERVER['SCRIPT_NAME']; 
+        $dir = dirname($scriptName);           
+        $dir = str_replace('\\', '/', $dir);   
+        if ($dir === '/') $dir = '';           
         $this->twig->addGlobal('base_url', $dir);
     }
 
