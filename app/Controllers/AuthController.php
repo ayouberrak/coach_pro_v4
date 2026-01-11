62df497b30b9562e15f1ab865ccd13f82ce6b9d2
ayouberrak <ayouberrak30@gmail.com>
2026-01-11 23:18:29 +0100

    [Controllers] Update AuthController

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,7 +2,12 @@
 namespace App\Controllers;
 
 use Core\Controller;
-use Services\AuthServices;
+
+use App\Models\User;
+use App\Models\Coach;
+use App\Models\Sportif;
+
+use App\Services\AuthServices;
 use App\Services\RolesServices;
 
 
@@ -12,9 +17,44 @@ class AuthController extends Controller {
         $this->render('auth/login.twig');
     }
 
-    public function register() {
+    public function showRegisterForm() {
         $roleService = new RolesServices();
         $roles = $roleService->getAllRoles();
         $this->render('auth/register.twig', ['roles' => $roles]);
     }
+    public function register($data) {
+        $coachData = null;
+        $sportifData = null;
+
+        $role =  (int)$data['role'];
+
+        if($role === 2){ 
+            $coachData = [
+                'biographie' => $data['biographie'] ?? '',
+                'photo' => $data['photo'] ?? '',
+                'annees_experience' => $data['annees_experience'] ?? 0,
+                'certification' => $data['certification'] ?? ''
+            ];
+        } elseif($role === 1){ 
+            $sportifData = [
+                'numero_telephone' => $data['telephone'] ?? ''
+            ];
+            
+        }
+
+        $authService = new AuthServices();
+
+        try {
+            $authService->registerUser($data, $coachData, $sportifData);
+            header("Location: ".BASE_URL."/login");
+            exit();
+        } catch (\Exception $e) {
+            $this->render('auth/register.twig', [
+                'error' => $e->getMessage(),
+                'data' => $data
+            ]);
+            echo "Registration failed: " . $e->getMessage();
+        }
+    }
+
 }
