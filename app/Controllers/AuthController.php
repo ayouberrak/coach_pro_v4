3fb4311a2019c774803ee7fa6e1128dbf5383f55
ayouberrak <ayouberrak30@gmail.com>
2026-01-12 11:43:03 +0100

    [Controllers] Update AuthController

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -13,10 +13,6 @@ use App\Services\RolesServices;
 
 class AuthController extends Controller {
     
-    public function login() {
-        $this->render('auth/login.twig');
-    }
-
     public function showRegisterForm() {
         $roleService = new RolesServices();
         $roles = $roleService->getAllRoles();
@@ -29,11 +25,23 @@ class AuthController extends Controller {
         $role =  (int)$data['role'];
 
         if($role === 2){ 
+            // $file_name = pathinfo($_FILES['photo']['name'], PATHINFO_FILENAME);
+            $file_extension = pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
+
+            $new_file_name = 'image_' . time() . '.' . $file_extension;
+
+            $upload_dir = $_SERVER['DOCUMENT_ROOT'] . '/coachPro_v3/public/uploads/';
+
+
+            $upload_path = $upload_dir . $new_file_name;
+
+            move_uploaded_file($_FILES['photo']['tmp_name'], $upload_path);
+            
             $coachData = [
                 'biographie' => $data['biographie'] ?? '',
-                'photo' => $data['photo'] ?? '',
-                'annees_experience' => $data['annees_experience'] ?? 0,
-                'certification' => $data['certification'] ?? ''
+                'photo' => $new_file_name,
+                'annee_experience' => $data['experience'] ?? 0,
+                'certefications' => $data['certification'] ?? ''
             ];
         } elseif($role === 1){ 
             $sportifData = [
@@ -56,5 +64,42 @@ class AuthController extends Controller {
             echo "Registration failed: " . $e->getMessage();
         }
     }
+
+    public function Login() {
+        $authService = new AuthServices();
+
+        try {
+            if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+                $this->render('auth/login.twig');
+                return;
+            }
+            $user = $authService->LoginUser($_POST['email'], $_POST['password']);
+            if ($user) {
+                session_start();
+                $_SESSION['user_id'] = $user->getId();
+                $_SESSION['user_role'] = $user->getRole();
+
+                if ($user->getRole() == 2) {
+                    header("Location: ".BASE_URL."/coach/dashboard");
+                } elseif ($user->getRole() == 1) {
+                    header("Location: ".BASE_URL."/sportif/dashboard");
+                } else {
+                    header("Location: ".BASE_URL."/login");
+                }
+                exit();
+            }else{
+                echo "invalid";
+            }
+        } catch (\Exception $e) {
+            $this->render('auth/login.twig', ['error' => 'error ' . $e->getMessage()]);
+        }
+    }
+    public function logout() {
+        session_start();
+        session_destroy();
+        header("Location: " . BASE_URL . "/login");
+        exit();
+    }
+
 
 }
