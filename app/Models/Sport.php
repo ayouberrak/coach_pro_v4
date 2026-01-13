f244e0c19259dc0fdfb542b340afc1d3d51c6e4e
ayouberrak <ayouberrak30@gmail.com>
2026-01-13 19:43:58 +0100

    [Models] Add Sport model

---
+++ app/Models/Sport.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+class Sport{
+    private ?int $idSport;
+    private string $type;
+    private string $description;
+
+    public function __construct(?int $idSport = null , string $type , string $description){
+        $this->idSport = $idSport;
+        $this->type = $type;
+        $this->description = $description;
+    }
+
+    public function getIdSport(){
+        return $this->idSport;
+    }
+    public function getType(){
+        return $this->type;
+    }
+
+    public function getDescription(){
+        return $this->description;
+    }
+
+    
+}
