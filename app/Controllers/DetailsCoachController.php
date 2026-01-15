ff3bbe7f7333136319ef3d37278b6c53264fef0b
ayouberrak <ayouberrak30@gmail.com>
2026-01-15 11:49:35 +0100

    fix(controller): update DetailsCoachController

--- app/Controllers/DetailsCoachController.php
+++ app/Controllers/DetailsCoachController.php
@@ -6,6 +6,8 @@ use Core\Controller;
 use App\Services\CoachServices;
 use App\Services\DispoServices;
 use App\Services\SportServices;
+use App\Services\ReservationServices;
+use Exception;
 class DetailsCoachController extends Controller {
 
         public function showCoaches($id) {
@@ -32,5 +34,36 @@ class DetailsCoachController extends Controller {
                 'disopFroma' => $disopFroma,
                 'sport' => $sport
             ]);
+    }
+
+    public function createReservation($request, $id) {
+        session_start();
+        \App\Middleware\AuthMiddleware::handleSportif();
+        $user_role = $_SESSION['user_role'] ;
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id_sportif = $_SESSION['user_id'];
+            $id_coach = (int) $id;
+            $heure_debut = $request['heure'];
+            $date_reservation = $request['date'];
+            $duree = $request['duree'];
+
+            $reservationServices = new ReservationServices();
+            try {
+                $reservationServices->createReservation([
+                    'id_client' => $id_sportif,
+                    'date_seance' => $date_reservation ,
+                    'debut' => $heure_debut,
+                    'duree' => $duree, 
+                    'id_status' => 3
+                ], $id_coach);
+                
+                header('Location: '.BASE_URL.'/sportif/coach/details/'.$id_coach);
+                exit();
+            } catch (Exception $e) {
+                echo "Error creating reservation: " . $e->getMessage();
+            }
+        }
+       
     }
 }
