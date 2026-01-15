05b2f726c6d7a204974a4d56c194e9a2165be3f7
ayouberrak <ayouberrak30@gmail.com>
2026-01-15 11:49:36 +0100

    fix(repository): update ReservationRepository

--- app/Repository/ReservationRepository.php
+++ app/Repository/ReservationRepository.php
@@ -17,13 +17,50 @@ class ReservationRepository {
 
     public function createReservation(Reservation $reservation): bool {
         try {
-            $stmt = $this->db->prepare("INSERT INTO reservation (id_seance, id_sportif, date_reservation) VALUES (:id_seance, :id_sportif, :date_reservation)");
-            $stmt->bindValue(':id_seance', $reservation->getIdSeance(), PDO::PARAM_INT);
-            $stmt->bindValue(':id_sportif', $reservation->getIdSportif(), PDO::PARAM_INT);
-            $stmt->bindValue(':date_reservation', $reservation->getDateReservation(), PDO::PARAM_STR);
+            $stmt = $this->db->prepare("INSERT INTO seances ( debut, duree , id_client , id_coach, id_status ,date_seance) VALUES (:debut, :duree, :id_client, :id_coach, :id_status, :date_seance)");
+            $stmt->bindValue(':debut', $reservation->getDebut(), PDO::PARAM_STR);
+            $stmt->bindValue(':duree', $reservation->getDuree(), PDO::PARAM_INT);
+            $stmt->bindValue(':id_client', $reservation->getIdClient(), PDO::PARAM_INT);
+            $stmt->bindValue(':id_coach', $reservation->getIdCoach(), PDO::PARAM_INT);
+            $stmt->bindValue(':id_status', $reservation->getIdStatus(), PDO::PARAM_INT);
+            $stmt->bindValue(':date_seance', $reservation->getDateSeance(), PDO::PARAM_STR);
             return $stmt->execute();
         } catch (Exception $e) {
             throw new Exception("Erreur lors de la création de la réservation: " . $e->getMessage());
+        }
+    }
+
+    public function getReservationsEnAttenteByIdClient(int $idClient): array {
+        try {
+            $stmt = $this->db->prepare("SELECT * FROM seances s
+                                                inner join users u on u.id = s.id_coach
+                                                inner join coach c on c.id_coach = u.id
+                                                WHERE id_client = :id_client
+                                                AND s.id_status != 4 ");
+
+            $stmt->bindValue(':id_client', $idClient, PDO::PARAM_INT);
+            $stmt->execute();
+            $reservationsData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $info = [];
+            foreach ($reservationsData as $data) {
+                $info[] = [
+                    'reservation' => new Reservation(
+                        $data['id_seance'],
+                        $data['id_client'],
+                        $data['id_coach'],
+                        $data['date_seance'],
+                        $data['debut'],
+                        $data['duree'],
+                        $data['id_status']
+                    ),
+                    'nomCoach' => $data['prenom'] . ' ' . $data['nom'],
+                    'coachPhoto' => $data['photo']
+                ];
+            }
+            return $info;
+        } catch (Exception $e) {
+            throw new Exception("Erreur lors de la récupération des réservations: " . $e->getMessage());
         }
     }
 
