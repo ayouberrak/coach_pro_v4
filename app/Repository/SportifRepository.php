846a94fa840b09e8e8f7a6b84d0322f75749f37c
ayouberrak <ayouberrak30@gmail.com>
2026-01-15 15:02:19 +0100

    fix(repository): update SportifRepository

--- app/Repository/SportifRepository.php
+++ app/Repository/SportifRepository.php
@@ -30,4 +30,14 @@ class SportifRepository {
             throw new \Exception("Erreur lors de la création du sportif: " . $e->getMessage());
         }
     }
+
+    public function getInfoClientById(int $id_client) {
+        $stmt = $this->db->prepare("SELECT * FROM users u inner join client c on c.id_client = u.id WHERE u.id = :id_client");
+        $stmt->bindParam(':id_client', $id_client, PDO::PARAM_INT);
+        $stmt->execute();
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($data) {
+            return Sportif::createFromArray($data);
+        }
+    }
 }
