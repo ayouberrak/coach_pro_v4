73854dc968fb32b1b223e0654a373c5550a28255
ayouberrak <ayouberrak30@gmail.com>
2026-01-15 15:02:19 +0100

    fix(controller): improve SportifController

--- app/Controllers/SportifController.php
+++ app/Controllers/SportifController.php
@@ -2,11 +2,15 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use App\Middleware\AuthMiddleware;
+use App\Services\ReservationServices;
 
 class SportifController extends Controller {
     public function index() {
         session_start();
-        \App\Middleware\AuthMiddleware::handleSportif();
-        $this->render('sportif/dashboard.twig');
+        AuthMiddleware::handleSportif();
+        $resServices = new ReservationServices();
+        $total = $resServices->countResByClient($_SESSION['user_id']);
+        $this->render('sportif/dashboard.twig', ['total' => $total]);
     }
 }
