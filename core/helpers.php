df880c315038134f1fd1fe1df01b5a6fd1999829
ayouberrak <ayouberrak30@gmail.com>
2026-01-15 11:49:36 +0100

    core: update helper functions

--- core/helpers.php
+++ core/helpers.php
@@ -6,4 +6,10 @@ class Helpers {
     public static function url($path = '') {
         return '/coachPro_v3' . $path;
     }
+
+    public static function checkDateTerminer($dateSeance) {
+        $currentDate = new \DateTime();
+        $seanceDate = new \DateTime($dateSeance);
+        return $seanceDate < $currentDate;
+    }
 }   
