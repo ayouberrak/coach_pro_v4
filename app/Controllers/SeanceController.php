6bfef8da1e1e0e9a18354254f1763f3b66e509a5
ayouberrak <ayouberrak30@gmail.com>
2026-01-15 11:49:35 +0100

    fix(controller): update SeanceController logic

--- app/Controllers/SeanceController.php
+++ app/Controllers/SeanceController.php
@@ -3,12 +3,23 @@ namespace App\Controllers;
 
 use \App\Middleware\AuthMiddleware;
 use Core\Controller;
+use App\Services\ReservationServices;
 
 class SeanceController extends Controller {
     public function index() {
         session_start();
+        // echo $_SESSION['user_id'];/
         $user_role = $_SESSION['user_role'] ;
         AuthMiddleware::handleSportif();
-        $this->render('sportif/seances.twig', ['user_role' => $user_role]);
+
+        $resServices = new ReservationServices();
+        $resServices = $resServices->getReservationsByClientId($_SESSION['user_id']);
+
+        // print_r($resServices);
+
+
+        $this->render('sportif/seances.twig', [
+                'user_role' => $user_role ,
+                'reservation' => $resServices]);
     }
 }
