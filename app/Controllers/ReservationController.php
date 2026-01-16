39de62f2da717ae7a85ece40b847669a81faed46
ayouberrak <ayouberrak30@gmail.com>
2026-01-16 11:32:20 +0100

    fix(controller): update ReservationController

--- app/Controllers/ReservationController.php
+++ app/Controllers/ReservationController.php
@@ -2,13 +2,37 @@
 namespace App\Controllers;
 
 use Core\Controller;
+use \App\Services\ReservationServices;
+use \App\Middleware\AuthMiddleware;
 
 class ReservationController extends Controller {
     public function index() {
         session_start();
-        \App\Middleware\AuthMiddleware::handleCoach();
+        AuthMiddleware::handleCoach();
          $user_role = $_SESSION['user_role'] ;
 
         $this->render('coach/reservations.twig', ['user_role' => $user_role]);
     }
+
+
+    public function acceptReservation($id) {
+        session_start();
+        AuthMiddleware::handleCoach();
+        $resService = new ReservationServices();
+        $resService->changeStatusReservation($id, 1);
+        header("Location: ".BASE_URL."/coach/dashboard");
+        exit();
+    }
+
+    public function declineReservation($id) {
+        session_start();
+        AuthMiddleware::handleCoach();
+        $resService = new ReservationServices();
+        $resService->changeStatusReservation($id, 2);
+        header("Location: ".BASE_URL."/coach/dashboard");
+        exit();
+        
+    }
+
+    
 }
