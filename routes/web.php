8bfcb86f13e44368c77d72d879edafcc1fd68c24
ayouberrak <ayouberrak30@gmail.com>
2026-01-16 11:32:22 +0100

    routes: update reservation and profile routes

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,19 @@ $router->get('/coach/reservations', [App\Controllers\ReservationController::clas
 
 $router->get('/coach/disponibilities', [App\Controllers\DispoController::class, 'index']);
 
-$router->get('/coach/dashboard', [App\Controllers\CoachController::class, 'index']);
+$router->get('/coach/dashboard', [App\Controllers\CoachController::class, 'dashboard']);
+
+$router->get('/coach/dashboard/accept/{id}', [App\Controllers\ReservationController::class, 'acceptReservation']);
+$router->get('/coach/dashboard/refuse/{id}', [App\Controllers\ReservationController::class, 'declineReservation']);
+
 
 
 $router->get('/sportif/dashboard', [App\Controllers\SportifController::class, 'index']);
 $router->get('/sportif/coaches', [App\Controllers\CoachsController::class, 'index']);
 $router->get('/sportif/seances', [App\Controllers\SeanceController::class, 'index']);
+
 $router->get('/sportif/profile', [App\Controllers\ProfileSportifController::class, 'showProfile']);
+$router->post('/sportif/profile', [App\Controllers\ProfileSportifController::class, 'editProfile']);
 
 $router->get('/sportif/coach/details/{id}', [App\Controllers\DetailsCoachController::class, 'showCoaches']);
 $router->post('/sportif/coach/details/{id}', [App\Controllers\DetailsCoachController::class, 'createReservation']);
